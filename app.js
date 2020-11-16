e918b7d05169708ae3056220485a66ffddf4b139
cuonghp92 <63277436+cuonghp92@users.noreply.github.com>
2020-11-16 11:58:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('<html>Hello world</html>'))
+app.get('/', (req, res) => res.send('<html>Hello Cuong</html>'))
 module.exports = app
